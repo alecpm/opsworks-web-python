cdfd676f2d48087bce0e01d80b56d99f4a0476ec
David Glick <david@glicksoftware.com>
2014-04-28 18:41:49 -0700

    generate nginx configuration for SSL termination if the plone app has SSL configured
    refactor ssl support into a separate recipe meant to be run on the deploy event
    
    include dependency
    
    add an attribute so I can add some additional servers for redirects
    
    SSL optimizations

--- plone_buildout/attributes/default.rb
+++ plone_buildout/attributes/default.rb
@@ -77,7 +77,9 @@ default["ebs_snapshots"]["aws_secret"] = nil
 
 # Nginx config options
 default['nginx_plone']['enable_ssi'] = false
+default['nginx_plone']['additional_servers'] = nil
 default['nginx_plone']['additional_config'] = nil
+default['nginx_plone']['additional_ssl_config'] = nil
 
 # Change default configs for other packages
 node.normal["redis"]["config"]["listen_addr"] = "0.0.0.0"
