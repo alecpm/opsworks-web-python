4c26a3bc2ea485f68bb5af1cfe2a2ecc8270348d
Alec Mitchell <alecpm@gmail.com>
2014-05-18 08:19:04 -0700

    Add default create before symlink dirs to avoid deploy errors.

--- opsworks_deploy_python/attributes/default.rb
+++ opsworks_deploy_python/attributes/default.rb
@@ -4,7 +4,7 @@ include_attribute "opsworks_deploy_python::buildout"
 node.default["deploy_python"]["custom_type"] = "python"
 node.default["deploy_python"]["symlink_before_migrate"] = {}
 node.default["deploy_python"]["purge_before_symlink"] = []
-node.default["deploy_python"]["create_dirs_before_symlink"] = []
+node.default["deploy_python"]["create_dirs_before_symlink"] = ['public', 'tmp']
 node.default["deploy_python"]["packages"] = []
 node.default["deploy_python"]["os_packages"] = []
 node.default["deploy_python"]["venv_options"] = '--no-site-packages'
