9559ae49834f0f6a6386562b1f8442533ad85398
Alec Mitchell <alecpm@gmail.com>
2014-06-08 18:23:05 -0700

    Run bin/develop co, in case we change source revisions or branches.

--- plone_buildout/recipes/instances-develop-up.rb
+++ plone_buildout/recipes/instances-develop-up.rb
@@ -4,10 +4,17 @@ deploy = node[:deploy][app_name]
 # Don't run if deploy hasn't been run, or there's no mr.developer
 return if !deploy || deploy.empty? || !deploy[:deploy_to] || !::File.exists?(deploy[:deploy_to]) || !::File.exists?(::File.join(deploy[:deploy_to], 'current'))
 
-develop_cmd =::File.join(deploy[:deploy_to], 'current', 'bin', 'develop')
-execute "#{develop_cmd} up" do
+develop_bin = ::File.join(deploy[:deploy_to], 'current', 'bin', 'develop')
+execute "#{develop_bin} co -a '.*'" do
+  user deploy[:user]
+  group deploy[:group]
+  cwd  ::File.join(deploy[:deploy_to], 'current')
+  only_if 'test -e #{develop_bin}'
+end
+
+execute "#{develop_bin} up" do
   user deploy[:user]
   group deploy[:group]
   cwd ::File.join(deploy[:deploy_to], 'current')
-  only_if "test -e #{develop_cmd}"
+  only_if "test -e #{develop_bin}"
 end
--- plone_buildout/recipes/instances-rebuild.rb
+++ plone_buildout/recipes/instances-rebuild.rb
@@ -8,6 +8,16 @@ include_recipe "plone_buildout::instances"
 # Update deploy
 deploy = node[:deploy][app_name]
 
+develop_bin = ::File.join(deploy[:deploy_to], 'current', 'bin', 'develop')
+if ::File.exist?(develop_bin)
+  execute "#{develop_bin} co -a '.*'" do
+    user deploy[:user]
+    group deploy[:group]
+    cwd  ::File.join(deploy[:deploy_to], 'current')
+    only_if 'test -e #{develop_bin}'
+  end
+end
+
 buildout_configure do
   deploy_data deploy
   app_name app_name
