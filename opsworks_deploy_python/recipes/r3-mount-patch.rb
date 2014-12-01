8cb071b53f04a108672d5c53b93ae3950c1200ec
Alec Mitchell <alecpm@gmail.com>
2014-12-01 01:09:25 -0800

    Properly escape quote in mount point check

--- opsworks_deploy_python/recipes/r3-mount-patch.rb
+++ opsworks_deploy_python/recipes/r3-mount-patch.rb
@@ -6,7 +6,7 @@ if node[:opsworks][:instance][:instance_type].start_with?('r3.')
     fs_dev = '/dev/xvdb'
     execute "mkfs" do
         command "mkfs.ext4 -E nodiscard #{fs_dev}"
-        not_if "blkid #{fs_dev} | grep TYPE=\"ext"
+        not_if "blkid #{fs_dev} | grep TYPE=\\\"ext"
     end
     execute "mount all" do
         command "mount -a"
