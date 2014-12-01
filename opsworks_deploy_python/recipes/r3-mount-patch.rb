415f97052b93c30791a523743333f0005be9ac66
Alec Mitchell <alecpm@gmail.com>
2014-11-30 23:44:53 -0800

    Force init recipe to reload, correct FS check.

--- opsworks_deploy_python/recipes/r3-mount-patch.rb
+++ opsworks_deploy_python/recipes/r3-mount-patch.rb
@@ -6,11 +6,13 @@ if node[:opsworks][:instance][:instance_type].start_with?('r3.')
     fs_dev = '/dev/xvdb'
     execute "mkfs" do
         command "mkfs.ext4 -E nodiscard #{fs_dev}"
-        not_if "blkid #{fs_dev} | grep -v TYPE=\"ext"
+        not_if "blkid #{fs_dev} | grep TYPE=\"ext"
     end
     execute "mount all" do
         command "mount -a"
     end
 end
 
+# Re-run base setup recipes
+node.run_state[:seen_recipes].delete("opsworks_initial_setup")
 include_recipe "opsworks_initial_setup"
