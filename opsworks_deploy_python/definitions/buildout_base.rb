d202964afec4165181156999520a757459dc9182
Alec Mitchell <alecpm@gmail.com>
2015-01-22 16:05:48 -0800

    Add ability to apply generic bootstrap parameters.

--- opsworks_deploy_python/definitions/buildout_base.rb
+++ opsworks_deploy_python/definitions/buildout_base.rb
@@ -52,13 +52,16 @@ define :buildout_configure do
   if deploy[:deploy_to] && (node[:deploy][application]["initially_deployed"] || ::File.exist?(deploy[:deploy_to]))
     release_path = ::File.join(deploy[:deploy_to], 'current')
     config_file = Helpers.buildout_setting(deploy,'config', node)
-    bootstrap_cmd = "#{::File.join(deploy[:deploy_to], "shared", "env", "bin", "python")} #{::File.join(".", "bootstrap.py")} -c #{config_file}"
-
+    bootstrap_cmd = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'python')} #{::File.join('.', 'bootstrap.py')} -c #{config_file}"
     buildout_cmd = ::File.join(release_path, "bin", "buildout")
     build_cmd = "#{buildout_cmd} -c #{config_file} #{Helpers.buildout_setting(deploy, 'flags', node)}"
     buildout_version = Helpers.buildout_setting(deploy,'buildout_version', node)
     if !(buildout_version.nil? || buildout_version.empty?)
       bootstrap_cmd = "#{bootstrap_cmd} -v #{buildout_version}"
+    end
+    bootstrap_params = Helpers.buildout_setting(deploy,'bootstrap_params', node)
+    if !(bootstrap_params.nil? || bootstrap_params.empty?)
+      bootstrap_cmd = "#{bootstrap_cmd} #{bootstrap_params}"
     end
     services = []
     # Add our anticipated services (or supervisor) to upstart or supervisor
