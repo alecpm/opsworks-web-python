2aa376b5b08a443a34636d34ce6b45658572041c
Alec Mitchell <alecpm@gmail.com>
2015-02-25 04:13:41 -0800

    Ensure mount points exist on new /mnt dir for r3 patch.

--- opsworks_deploy_python/recipes/r3-mount-patch.rb
+++ opsworks_deploy_python/recipes/r3-mount-patch.rb
@@ -12,13 +12,24 @@ if node[:opsworks][:instance][:instance_type].start_with?('r3.')
         command "umount -a"
         ignore_failure true
     end
+    execute "mount /mnt" do
+        command "mount /mnt"
+        ignore_failure true
+    end
+    for dir in [node["plone_zeoserver"]["filestorage_dir"], node["plone_blobs"]["blob_dir"], node["plone_solr"]["data_dir"], "/mnt/srv/www"]
+        if !dir.nil? && !dir.empty? && !::File.exists?(dir)
+            directory dir do
+                user 'deploy'
+                group 'www-data'
+                mode 0700
+                recursive true
+                action :create
+            end
+        end
+    end
     execute "mount all" do
         command "mount -a"
         ignore_failure true
-        only_if do
-            sleep 5
-            true
-        end
     end
     # Re-run base setup recipes
     loaded_recipes = run_context.instance_variable_get(:@loaded_recipes)
