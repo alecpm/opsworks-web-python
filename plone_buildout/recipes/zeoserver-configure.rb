b81845d535f4c33725d8e6c3c77b79304672f66c
Alec Mitchell <alecpm@gmail.com>
2015-02-25 00:12:39 -0800

    A couple more stray bad nil checks.

--- plone_buildout/recipes/zeoserver-configure.rb
+++ plone_buildout/recipes/zeoserver-configure.rb
@@ -1,5 +1,5 @@
 app_name = node["plone_zeoserver"]["app_name"]
-return if !app_name
+return if app_name.nil? || app_name.empty?
 
 deploy = node[:deploy][app_name]
 # Don't run configure if deploy hasn't been run
