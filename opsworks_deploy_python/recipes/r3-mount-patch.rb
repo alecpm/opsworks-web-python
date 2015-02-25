177ca11c9bf8c5ed8018f3932f6167a8a955ac96
Alec Mitchell <alecpm@gmail.com>
2015-02-25 02:07:21 -0800

    More mount time fixes.

--- opsworks_deploy_python/recipes/r3-mount-patch.rb
+++ opsworks_deploy_python/recipes/r3-mount-patch.rb
@@ -15,9 +15,13 @@ if node[:opsworks][:instance][:instance_type].start_with?('r3.')
     execute "mount all" do
         command "mount -a"
         ignore_failure true
+        only_if sleep 5
     end
     # Re-run base setup recipes
     loaded_recipes = run_context.instance_variable_get(:@loaded_recipes)
     loaded_recipes.delete('opsworks_initial_setup::bind_mounts')
     include_recipe "opsworks_initial_setup::bind_mounts"
+    service "autofs" do
+        action :restart
+    end
 end
