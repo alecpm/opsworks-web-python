1490d9b0b083253a1cdca8a86179e809742f36da
Alec Mitchell <alecpm@gmail.com>
2015-02-25 01:29:11 -0800

    Ensure our remounts happen.

--- opsworks_deploy_python/recipes/r3-mount-patch.rb
+++ opsworks_deploy_python/recipes/r3-mount-patch.rb
@@ -8,12 +8,16 @@ if node[:opsworks][:instance][:instance_type].start_with?('r3.')
         command "mkfs.ext4 -E nodiscard #{fs_dev}"
         not_if "blkid #{fs_dev} | grep TYPE=\\\"ext"
     end
+    execute "umount all" do
+        command "umount -a"
+        ignore_failure true
+    end
     execute "mount all" do
         command "mount -a"
+        ignore_failure true
     end
+    # Re-run base setup recipes
+    loaded_recipes = run_context.instance_variable_get(:@loaded_recipes)
+    loaded_recipes.delete('opsworks_initial_setup::bind_mounts')
+    include_recipe "opsworks_initial_setup::bind_mounts"
 end
-
-# Re-run base setup recipes
-loaded_recipes = run_context.instance_variable_get(:@loaded_recipes)
-loaded_recipes.delete('opsworks_initial_setup::bind_mounts')
-include_recipe "opsworks_initial_setup::bind_mounts"
