dd40322945b68f970d4062624f2826a647e57b62
Alec Mitchell <alecpm@gmail.com>
2019-02-21 15:08:07 -0800

    Handle errors.

--- opsworks_initial_setup/recipes/replace_os_version.rb
+++ opsworks_initial_setup/recipes/replace_os_version.rb
@@ -1,9 +1,14 @@
-if File.readlines("/etc/lsb-release").grep(/pretending to be 14\.04/).size > 0
-    file "/etc/lsb-release" do
-        content 'DISTRIB_ID=Ubuntu\nDISTRIB_RELEASE=18.04\nDISTRIB_CODENAME=bionic\nDISTRIB_DESCRIPTION="Ubuntu 18.04.2 LTS"'
-        owner 'root'
-        group 'root'
-        mode '0644'
-        action :create
+begin
+    if File.readlines("/etc/lsb-release").grep(/pretending to be 14\.04/).size > 0
+        file "/etc/lsb-release" do
+            content 'DISTRIB_ID=Ubuntu\nDISTRIB_RELEASE=18.04\nDISTRIB_CODENAME=bionic\nDISTRIB_DESCRIPTION="Ubuntu 18.04.2 LTS"'
+            owner 'root'
+            group 'root'
+            mode '0644'
+            action :create
+            ignore_failure true
+        end
     end
+rescue
+    # ignore
 end
