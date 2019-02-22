7722e64bc5f1734590091da57eb25b4ea22cd5b9
Alec Mitchell <alecpm@gmail.com>
2019-02-22 12:26:01 -0800

    Try monkey patching chef provider.

--- opsworks_initial_setup/recipes/replace_os_version.rb
+++ opsworks_initial_setup/recipes/replace_os_version.rb
@@ -1,7 +1,10 @@
+# Patch Chef service provider for ubuntu
+Chef::Platform::platforms[:ubuntu][">= 16.04"] = {:service=>Chef::Provider::Service::Systemd}
 begin
     if File.readlines('/etc/lsb-release').grep(/pretending to be 14\.04/).size > 0
+        Chef::Platform::platforms[:ubuntu][">= 14.04"] = {:service=>Chef::Provider::Service::Systemd}
         file '/etc/lsb-release' do
-            content "DISTRIB_ID=Ubuntu\nDISTRIB_RELEASE=18.04\nDISTRIB_CODENAME=bionic\nDISTRIB_DESCRIPTION=\"Ubuntu 18.04.2 LTS pretending to be 14.04\""
+            content "DISTRIB_ID=Ubuntu\nDISTRIB_RELEASE=18.04\nDISTRIB_CODENAME=bionic\nDISTRIB_DESCRIPTION=\"Ubuntu 18.04.2 LTS pretending to be 14.04\"\n"
             owner 'root'
             group 'root'
             mode '0644'
