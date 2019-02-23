fd3f24863583bcc10d9d406f343124a7edd5cae9
Alec Mitchell <alecpm@gmail.com>
2019-02-22 17:12:54 -0800

    Fix redis

--- plone_buildout/recipes/redis.rb
+++ plone_buildout/recipes/redis.rb
@@ -1,7 +1,13 @@
 package 'redis-server'
 
+service 'redis-server'
+    supports :restart => true, :status => true, :reload => false
+    action :nothing
+end
+
 replace_or_add "Redis Listens on all addresses" do
     path "/etc/redis/redis.conf"
     pattern ".*bind 127\.0\.0\.1.*"
     line "bind 0.0.0.0"
+    notifies :restart, 'service[redis-server]'
 end
