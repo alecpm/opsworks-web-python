11c4fdf027c012beedef5eb4d12d20a1494d5f4e
Alec Mitchell <alecpm@gmail.com>
2019-02-23 09:25:56 -0800

    More authentic lsb-release.

--- opsworks_initial_setup/recipes/replace_os_version.rb
+++ opsworks_initial_setup/recipes/replace_os_version.rb
@@ -3,7 +3,7 @@ Chef::Platform::platforms[:ubuntu][">= 16.04"] = {:service=>Chef::Provider::Serv
 if node.normal['pretend_ubuntu_version']
     Chef::Platform::platforms[:ubuntu][">= 14.04"] = {:service=>Chef::Provider::Service::Systemd}
     file '/etc/lsb-release' do
-        content "DISTRIB_ID=Ubuntu\nDISTRIB_RELEASE=18.04\nDISTRIB_CODENAME=bionic\nDISTRIB_DESCRIPTION=\"Ubuntu 18.04.2 LTS pretending to be 14.04\"\n"
+        content "DISTRIB_ID=Ubuntu\nDISTRIB_RELEASE=18.04\nDISTRIB_CODENAME=bionic\nDISTRIB_DESCRIPTION=\"Ubuntu 18.04.2 LTS pretending to be 14.04\""
         owner 'root'
         group 'root'
         mode '0644'
