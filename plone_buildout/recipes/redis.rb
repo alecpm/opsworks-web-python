627958023ed58d8728897709661cdd30f7605d58
Alec Mitchell <alecpm@gmail.com>
2019-02-22 17:44:06 -0800

    Fix redis customization.

--- plone_buildout/recipes/redis.rb
+++ plone_buildout/recipes/redis.rb
@@ -8,7 +8,7 @@ end
 
 replace_or_add "Redis Listens on all addresses" do
     path "/etc/redis/redis.conf"
-    pattern ".*bind 127\.0\.0\.1.*"
+    pattern "^bind 127\.0\.0\.1.*"
     line "bind 0.0.0.0"
     notifies :restart, 'service[redis-server]'
 end
