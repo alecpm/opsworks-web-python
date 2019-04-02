ccea20c65f44c9c06562eaa1c4abb3dbb4fda16c
Alec Mitchell <alecpm@gmail.com>
2019-04-02 11:06:07 -0700

    Add secondary address option.

--- plone_buildout/attributes/default.rb
+++ plone_buildout/attributes/default.rb
@@ -153,6 +153,8 @@ if node['pretend_ubuntu_version'] || (platform?('ubuntu') && node['platform_vers
     node.normal['varnish']['reload_cmd'] = '/usr/share/varnish/varnishreload'
     node.normal['varnish']['path_to_vcl'] = "#{node['varnish']['dir']}/#{node['varnish']['vcl_conf']}"
     node.normal['varnish']['instance_name'] = "#{node['hostname']}"
+    node.normal['varnish']['secondary_listen_address'] = nil
+    node.normal['varnish']['secondary_listen_port'] = nil
 else
     node.normal["varnish"]["vcl_source"] = 'default.vcl.erb'
 end
