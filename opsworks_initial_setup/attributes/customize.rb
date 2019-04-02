1457c6616e87b7ed722717e33de9e323bfae3af6
Alec Mitchell <alecpm@gmail.com>
2019-04-02 09:06:49 -0700

    Add inotify limit increase

--- opsworks_initial_setup/attributes/customize.rb
+++ opsworks_initial_setup/attributes/customize.rb
@@ -1,1 +1,2 @@
 normal[:opsworks_initial_setup][:bind_mounts][:mounts]['/var/log/nginx'] = "#{node[:opsworks_initial_setup][:ephemeral_mount_point]}/var/log/nginx"
+normal[:opsworks_initial_setup][:sysctl]['fs.inotify.max_user_watches'] = 262144
