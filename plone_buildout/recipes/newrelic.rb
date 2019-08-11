c7439d5ad61a6dc04d783a0798c5a790514bd823
Alec Mitchell <alecpm@gmail.com>
2019-08-10 20:46:59 -0700

    Change default install method for certbot.

--- plone_buildout/recipes/newrelic.rb
+++ plone_buildout/recipes/newrelic.rb
@@ -1,3 +1,5 @@
+# Force tarball based on install for certbot, since it's not available via apt on 14.04
+node.normal['certbot']['install_method'] = 'certbot-auto'
 node.normal['newrelic']['server_monitoring']['license'] = node['newrelic']['license']
 node.normal['newrelic']['application_monitoring']['license'] = node['newrelic']['license']
 #node.normal['newrelic_meetme_plugin']['license'] = node['newrelic']['license']
