1dfaf4d6afc3b4c69e0c724cf5d83d7cbfbc8df8
Alec Mitchell <alecpm@gmail.com>
2021-04-24 14:28:05 -0700

    Turn on HTTP/2 optmizations if HTTP/2 is enabled.

--- opsworks_initial_setup/recipes/default.rb
+++ opsworks_initial_setup/recipes/default.rb
@@ -3,7 +3,11 @@ if node[:ec2] && (node[:ec2][:instance_type] == 't1.micro' ||
                   node['opsworks_initial_setup']['swapfile_instancetypes'] && node['opsworks_initial_setup']['swapfile_instancetypes'].include?(node[:ec2][:instance_type]))
   include_recipe 'opsworks_initial_setup::swap'
 end
-
+if node['nginx_plone']['enable_http2']
+  node[:sysctl]['net.core.default_qdisc'] = 'fq'
+  node[:sysctl]['net.ipv4.tcp_congestion_control'] = 'bbr'
+  node[:sysctl]['net.ipv4.tcp_notsent_lowat'] = '16384'
+end
 include_recipe 'opsworks_initial_setup::sysctl'
 include_recipe 'opsworks_initial_setup::limits'
 if infrastructure_class?('ec2')
